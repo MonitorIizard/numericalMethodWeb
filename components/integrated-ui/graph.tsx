7e7a1c24c0f2a0e7b09cd9ccb207159b0ba4e2d5
Salamander <mokmaard646@gmail.com>
2023-10-30 08:15:43 +0700

    integration add graph

---
+++ components/integrated-ui/graph.tsx
@@ -0,0 +1,43 @@
+import React from 'react';
+import dynamic from 'next/dynamic';
+import Point from '@/pages/interpolation/class/Point';
+import { Data} from 'plotly.js';
+const Plot = dynamic(() => import("react-plotly.js"), { ssr: false, })
+
+type GraphProps = {
+  graph?: Point[];
+}
+
+export default function Graph({graph} : GraphProps) {
+  const dataProps : Data[] = [];
+  // const dataProps : Data[] = Array.from({length : largrangGraph?.length ? largrangGraph?.length : 0 }, (v, i) => {
+  //   return { 
+  //   x: largrangGraph![i].map((point) => point.x[0]),
+  //   y: largrangGraph![i].map((point) => point.y),
+  //   name: `f(x) = L${i+1}*f(${i+1})`,
+  //   type: 'scatter',
+  //   marker: {color: `${Math.floor(Math.random() * 0xffffff).toString(16)}`},
+  // }
+  
+  return (
+    <div>
+       <Plot
+        data={dataProps.concat([
+          {
+            x: graph?.map((point) => point.x[0]),
+            y: graph?.map((point) => point.y),
+            name: 'Graph',
+            mode: 'lines',
+            type: 'scatter',
+            fill: 'tonexty',
+            marker: {color: 'red', size : 12},
+            line : {width : 6}
+          }
+        ])
+        }
+
+        layout={ {width: 600, height: 600, title: 'Graph🤯'} }
+      />
+    </div>
+  )
+}
--- pages/integration/simpson.tsx
+++ pages/integration/simpson.tsx
@@ -3,12 +3,16 @@ import { useEffect, useState } from 'react'
 import Simson from './class/Simpson';
 import Input from '../../components/integrated-ui/input'
 import ShowSolution from '@/components/integrated-ui/show-solution';
+import Graph from '@/components/integrated-ui/graph';
+import Point from '../interpolation/class/Point';
+import { evaluate } from 'mathjs';
 
 export default function Page() {
   const [data, setData] = useState<InputData>();
   const [answer, setAnswer] = useState<number[]>([]); 
   const [error, setError] = useState<number>(0);
   const [realValue, setRealValue] = useState<number>(0);
+  const [graph, setGraph] = useState<Point[]>([]);
   let solver = new Simson(data!);
 
   useEffect(() => {
@@ -16,14 +20,31 @@ export default function Page() {
     const {error : e, realValue : r} = solver.calError();
     setError(e);
     setRealValue(r);
+    setGraph(() => {
+      const equation = data?.equation;
+      const xStart = data?.xStart;
+      const xEnd = data?.xEnd;
+      const next:Point[] = [];
+      let step = Math.floor(Math.log(Math.abs(xEnd! - xStart!))/Math.log(10));
+      step = Math.pow(10, step - 1);
+
+      for ( let x = xStart!; x <= xEnd!; x += step) {
+        let y = evaluate(equation!, {x});
+        next.push(new Point([x], y));
+      }
+
+      return next;
+    })
   }, [data])
 
   return (
-  <div className="flex flex-col items-center">
+  <div className="flex flex-col items-center gap-4">
       <h1 className="text-3xl text-center font-bold py-4">Simpson</h1>
       <Input setData={setData}/>
 
       <ShowSolution answer={answer} realValue={realValue} error={error}/>
+
+      <Graph graph={graph}/>
   </div>
   )
 }
